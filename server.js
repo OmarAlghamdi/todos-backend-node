56bd8be0bf4b38c08ed9c22490bae7383c0ae0bf
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2021-06-10 03:02:18 +0300

    Enable https

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
 
 const auth = require('./routers/auth')
 const todos = require('./routers/todos')
@@ -16,5 +18,12 @@ app.use('/api/todos', todos)
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
+const HTTPS_PORT = process.env.HTTPS_PORT || 433
 app.listen(PORT, console.log(`server's been started on port ${PORT}`))
+
+https.createServer({
+    key: fs.readFileSync(__dirname + '/ssl/selfsigned.key'),
+    cert: fs.readFileSync(__dirname + '/ssl/selfsigned.crt')
+}, app)
+    .listen(HTTPS_PORT, console.log(`https is available on port ${HTTPS_PORT}`))
