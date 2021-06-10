01dd92bfddf499444bd4e6de8f423e8c51bd879f
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2021-06-10 03:31:24 +0300

    Redirect http to https

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const https = require('https')
+const http = require('http')
 const fs = require('fs')
 
 const auth = require('./routers/auth')
 const todos = require('./routers/todos')
+
+const PORT = process.env.PORT || 80
+const HTTPS_PORT = process.env.HTTPS_PORT || 433
 
 require('./sequelize')
 const errorHandler = require('./middlewares/errorHandler')
@@ -18,9 +22,12 @@ app.use('/api/todos', todos)
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 80
-const HTTPS_PORT = process.env.HTTPS_PORT || 433
-app.listen(PORT, console.log(`server's been started on port ${PORT}`))
+http.createServer((req, res) => {
+    const [host,] = req.headers.host.split(':')
+    console.log(host);
+    res.writeHead(301, { "Location": `https://${host}:${HTTPS_PORT}${req.url}`});
+    res.end();
+}).listen(PORT, console.log(`http is available on port ${PORT}\nbut all request will be redirected to https on port ${HTTPS_PORT}`))
 
 https.createServer({
     key: fs.readFileSync(__dirname + '/ssl/selfsigned.key'),
